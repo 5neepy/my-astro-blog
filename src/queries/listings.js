19e351d85f63fd6feac25dbb5f10fb633a6ccdc6
5neepy <martilambov@gmail.com>
2024-01-28 01:21:40 +0200

    finalize the listing

--- src/queries/listings.js
+++ src/queries/listings.js
@@ -1,13 +1,50 @@
 import GraphQL from "../scripts/graphql.js";
 
 export const listingList = await GraphQL.query(`
-query{
+query {
   listings {
-    data{
-      attributes{
+    data {
+      attributes {
+        biz_name
         slug
-        name
-        text1
+        seo_title
+        seo_desc
+        main_value
+        mv_info
+        cta_1
+        klient_img {
+          data {
+            attributes {
+              url
+              alternativeText
+            }
+          }
+        }
+        sc_hook
+        ico1
+        bnf1
+        bnf1_info
+        ico2
+        bnf2
+        bnf2_info
+        ico3
+        bnf3
+        bnf3_info
+        nas_title
+        nas_info
+        cta_2
+        img_me {
+          data {
+            attributes {
+              url
+              alternativeText
+            }
+          }
+        }
+        features_title
+        ft
+        faq_title
+        faq
       }
     }
   }
